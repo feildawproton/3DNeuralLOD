a639d674808561c12c9654447d619ee57ff83b0b
feildawproton <56926839+feildawproton@users.noreply.github.com>
2022-01-10 12:07:46 -0500

    Add files via upload

--- sample_mesh.py
+++ sample_mesh.py
@@ -2,12 +2,8 @@ import numpy as np
 from stl import mesh
 import datetime
 
-from compute_intersections import intersections_z
+from compute_inside import inside_z
 
-def sample_nonparallel(faces, points, inside_mesh_results):
-	for ndx in range(points.shape[0]):
-		intersections = intersections_z(points[ndx], faces)
-		result = intersections % 2
 
 def sample_mesh(stl_mesh, num_samples):
 	#create the sample coordinates
@@ -21,7 +17,7 @@ def sample_mesh(stl_mesh, num_samples):
 	zee_min = np.amin(zees)
 	zee_max = np.amax(zees)
 	
-	points = np.random.rand(1000, 3)
+	points = np.random.rand(num_samples, 3)
 	points[:,0] = points[:,0] * (exe_max - exe_min)
 	points[:,0] = points[:,0] + exe_min
 	points[:,1] = points[:,1] * (why_max - why_min)
@@ -29,8 +25,10 @@ def sample_mesh(stl_mesh, num_samples):
 	points[:,2] = points[:,2] * (zee_max - zee_min)
 	points[:,2] = points[:,2] + zee_min
 	
-	inside_mesh_results = np.zeros(points.shape[0])
+	inside_results = np.zeros(points.shape[0])
+	faces = np.ascontiguousarray(stl_mesh.vectors)
+	
 	start_time = datetime.datetime.now()
-	sample_nonparallel(stl_mesh.vectors, points, inside_mesh_results)
+	inside_z(points, faces, inside_results)
 	end_time = datetime.datetime.now()
 	print(end_time - start_time)
--- test_stl.py
+++ test_stl.py
@@ -4,90 +4,22 @@ from stl import mesh
 from mpl_toolkits import mplot3d
 from matplotlib import pyplot
 
-figure = pyplot.figure()
-axes = mplot3d.Axes3D(figure)
+if __name__ == "__main__":
 
-the_mesh = mesh.Mesh.from_file(os.path.join("data", "suzanne.stl"))
+	figure = pyplot.figure()
+	axes = mplot3d.Axes3D(figure)
 
-axes.add_collection3d(mplot3d.art3d.Poly3DCollection(the_mesh.vectors))
+	the_mesh = mesh.Mesh.from_file(os.path.join("data", "suzanne.stl"))
 
-#have to scale the mesh
-scale = the_mesh.points.flatten()
-axes.auto_scale_xyz(scale, scale, scale)
+	axes.add_collection3d(mplot3d.art3d.Poly3DCollection(the_mesh.vectors))
 
-pyplot.show()
+	#have to scale the mesh
+	scale = the_mesh.points.flatten()
+	axes.auto_scale_xyz(scale, scale, scale)
 
-#from compute_intersections import intersections_z, intersections_z_alt, intersections_z_nonparallel
-from sample_mesh import sample_mesh
+	pyplot.show()
 
-sample_mesh(the_mesh, 100)
+	#from compute_intersections import intersections_z, intersections_z_alt, intersections_z_nonparallel
+	from sample_mesh import sample_mesh
 
-'''
-# -- TEST CORRECTNESS -- 
-exes	= the_mesh.vectors[:,0]
-exe_min = np.amin(exes)
-exe_max = np.amax(exes)
-whys 	= the_mesh.vectors[:,1]
-why_min = np.amin(whys)
-why_max = np.amax(whys)
-zees 	= the_mesh.vectors[:,2]
-zee_min = np.amin(zees)
-zee_max = np.amax(zees)
-iter = 0
-while iter < 1000:
-	point = (np.random.uniform(exe_min, exe_max), np.random.uniform(why_min, why_max), np.random.uniform(zee_min, zee_max))
-
-	intersections_nonparallel = intersections_z_nonparallel(point, the_mesh.vectors)
-	intersections = intersections_z(point, the_mesh.vectors)
-	intersections_alt = intersections_z_alt(point, the_mesh.vectors)
-	if intersections_nonparallel != intersections or intersections != intersections_alt or intersections_alt != intersections_nonparallel:
-		print(intersections_nonparallel, intersections, intersections_alt)
-	iter += 1
-print("done checking correctness")
-'''
-'''
-# -- TESTING PERFORMANCE OF INTERSECTION CALCULATIONS --
-#the cuda versions are way faster
-# i think the first implementation may be faster than the alt
-num_iter = 10000
-exes	= the_mesh.vectors[:,0]
-exe_min = np.amin(exes)
-exe_max = np.amax(exes)
-whys 	= the_mesh.vectors[:,1]
-why_min = np.amin(whys)
-why_max = np.amax(whys)
-zees 	= the_mesh.vectors[:,2]
-zee_min = np.amin(zees)
-zee_max = np.amax(zees)
-
-start_time = datetime.datetime.now()
-iter = 0
-while iter < num_iter:
-	point = (np.random.uniform(exe_min, exe_max), np.random.uniform(why_min, why_max), np.random.uniform(zee_min, zee_max))
-	intersections = intersections_z_nonparallel(point, the_mesh.vectors)
-	iter += 1
-end_time = datetime.datetime.now()
-print("done testing nonparallel time")
-print(end_time - start_time)
-
-start_time = datetime.datetime.now()
-iter = 0
-while iter < num_iter:
-	point = (np.random.uniform(exe_min, exe_max), np.random.uniform(why_min, why_max), np.random.uniform(zee_min, zee_max))
-	intersections = intersections_z(point, the_mesh.vectors)
-	iter += 1
-end_time = datetime.datetime.now()
-print("done testing GPU parallel time")
-print(end_time - start_time)
-
-start_time = datetime.datetime.now()
-iter = 0
-while iter < num_iter:
-	point = (np.random.uniform(exe_min, exe_max), np.random.uniform(why_min, why_max), np.random.uniform(zee_min, zee_max))
-	intersections = intersections_z_alt(point, the_mesh.vectors)
-	iter += 1
-end_time = datetime.datetime.now()
-print("done testing GPU parallel time")
-print(end_time - start_time)
-'''
-
+	sample_mesh(the_mesh, 1000)
