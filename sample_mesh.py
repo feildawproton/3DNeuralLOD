818a0575c50a308d1ca62a8557d35ecec8fc96e1
Alexander Feild <feildaw@protonmail.com>
2022-01-11 10:02:48 -0500

    update

--- sample_mesh.py
+++ sample_mesh.py
@@ -6,16 +6,19 @@ from compute_inside import inside_z
 
 
 def sample_mesh(stl_mesh, num_samples):
+	print("sampling mesh with %i samples" % num_samples)
 	#create the sample coordinates
-	exes	= stl_mesh.vectors[:,0]
+	exes	= stl_mesh.vectors[:,:,0]
 	exe_min = np.amin(exes)
 	exe_max = np.amax(exes)
-	whys 	= stl_mesh.vectors[:,1]
+	whys 	= stl_mesh.vectors[:,:,1]
 	why_min = np.amin(whys)
 	why_max = np.amax(whys)
-	zees 	= stl_mesh.vectors[:,2]
+	zees 	= stl_mesh.vectors[:,:,2]
 	zee_min = np.amin(zees)
 	zee_max = np.amax(zees)
+	
+	print(exe_min, exe_max, why_min, why_max, zee_min, zee_max)
 	
 	points = np.random.rand(num_samples, 3)
 	points[:,0] = points[:,0] * (exe_max - exe_min)
@@ -32,3 +35,53 @@ def sample_mesh(stl_mesh, num_samples):
 	inside_z(points, faces, inside_results)
 	end_time = datetime.datetime.now()
 	print(end_time - start_time)
+	
+	return points, inside_results
+
+#this works by taking the face centers, scaling them up and down, and using all those as the samples
+def sample_mesh_selective(stl_mesh):
+	points = np.zeros((stl_mesh.vectors.shape[0], stl_mesh.vectors.shape[2]), dtype = np.float32)
+	for ndx in range(points.shape[0]):
+		avg = stl_mesh.vectors[ndx][0] + stl_mesh.vectors[ndx][1] + stl_mesh.vectors[ndx][2]
+		avg = avg * (1/3)
+		points[ndx] = avg
+	scaled_down = points * 0.9
+	scale_up = points * 1.1
+	print(points.shape, points.dtype)
+	print(scaled_down.shape, scaled_down.dtype)
+	print(scale_up.shape, scale_up.dtype)
+	points = np.concatenate((points, scaled_down, scale_up))
+	'''
+	#print("sampling mesh with %i samples" % num_samples)
+	#create the sample coordinates
+	exes	= stl_mesh.vectors[:,:,0]
+	exe_min = np.amin(exes)
+	exe_max = np.amax(exes)
+	whys 	= stl_mesh.vectors[:,:,1]
+	why_min = np.amin(whys)
+	why_max = np.amax(whys)
+	zees 	= stl_mesh.vectors[:,:,2]
+	zee_min = np.amin(zees)
+	zee_max = np.amax(zees)
+	
+	print(exe_min, exe_max, why_min, why_max, zee_min, zee_max)
+	
+	num_samples = points.shape[0]
+	
+	points = np.random.rand(num_samples, 3)
+	points[:,0] = points[:,0] * (exe_max - exe_min)
+	points[:,0] = points[:,0] + exe_min
+	points[:,1] = points[:,1] * (why_max - why_min)
+	points[:,1] = points[:,1] + why_min
+	points[:,2] = points[:,2] * (zee_max - zee_min)
+	points[:,2] = points[:,2] + zee_min
+	'''
+	inside_results = np.zeros(points.shape[0])
+	faces = np.ascontiguousarray(stl_mesh.vectors)
+	
+	start_time = datetime.datetime.now()
+	inside_z(points, faces, inside_results)
+	end_time = datetime.datetime.now()
+	print(end_time - start_time)
+	
+	return points, inside_results
--- test_stl.py
+++ test_stl.py
@@ -2,24 +2,52 @@ import os
 import numpy as np
 from stl import mesh
 from mpl_toolkits import mplot3d
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
+from matplotlib.pylab import cm
+from matplotlib.colors import ListedColormap
+
+from sample_mesh import sample_mesh, sample_mesh_selective
 
 if __name__ == "__main__":
 
-	figure = pyplot.figure()
+	figure = plt.figure()
 	axes = mplot3d.Axes3D(figure)
 
-	the_mesh = mesh.Mesh.from_file(os.path.join("data", "suzanne.stl"))
-
+	the_mesh = mesh.Mesh.from_file(os.path.join("data", "makehumanmale.stl"))
+	'''
 	axes.add_collection3d(mplot3d.art3d.Poly3DCollection(the_mesh.vectors))
 
 	#have to scale the mesh
 	scale = the_mesh.points.flatten()
 	axes.auto_scale_xyz(scale, scale, scale)
 
-	pyplot.show()
+	plt.show()
+	'''
 
 	#from compute_intersections import intersections_z, intersections_z_alt, intersections_z_nonparallel
-	from sample_mesh import sample_mesh
+	
+	print(the_mesh.vectors.shape)
+	
+	#points, labels = sample_mesh(the_mesh, 1000)
+	
+	points, labels = sample_mesh_selective(the_mesh)
+	
+	print(labels.shape)
+	
+	#transparent colormap from https://riptutorial.com/matplotlib/example/11646/using-custom-colormaps
+	cm.register_cmap(name="purple_transparency",
+                 data={'red':   [(0.,0.4,0.4),
+                                 (1.,0.4,0.4)],
 
-	sample_mesh(the_mesh, 1000)
+                       'green': [(0.,0.0,0.0),
+                                 (1.,0.0,0.0)],
+
+                       'blue':  [(0.,1,1),
+                                 (1.,1,1)],
+
+                       'alpha': [(0.,0,0),
+                                 (1,1,1)]})
+
+	#axes.scatter3D(points[:,0], points[:,1], points[:,2], c=labels, cmap = "purple_transparency")
+	axes.scatter3D(points[:,0], points[:,1], points[:,2], c=labels)
+	plt.show()
